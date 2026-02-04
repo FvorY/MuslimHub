010676c75cfd3e965bcc58678304d1f45550ad11
ferdycp <ferdyp73@gmail.com>
2026-02-04 21:18:17 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muslimhub.app',
+  appName: 'MuslimHub1',
+  webDir: 'dist'
+};
+
+export default config;
